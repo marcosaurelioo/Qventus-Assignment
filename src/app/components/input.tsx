c09c3297600c54cd514285b02c802e40a074fd06
Marcos Aurélio <marcosaureliolelo@outlook.com>
2023-12-10 22:53:55 -0300

    feat: implementing input requirements logic

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,32 +1,55 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
-const array = [
-  { name: "Has a number 0-9" },
-  { name: "Has a special char !@$$%^&*" },
-  { name: "Has a uppercase Letter" },
-  { name: "Has NO consecutive letters" },
-];
+type defaultRequirements =
+  | "consecutiveLetters"
+  | "specialChar"
+  | "uppercaseLetter"
+  | "hasNumber";
 
-export function Input() {
+interface InputProps {
+  manualRequirement?: [{ name: string; regex: RegExp }] | [];
+  requirements?: defaultRequirements[] | [];
+}
+
+const defaultPasswordRequirements = {
+  hasNumber: { name: "Has a number 0-9", regex: /\d/ },
+  specialChar: { name: "Has a special char !@$$%^&*", regex: /[!@#$%^&*]/ },
+  uppercaseLetter: { name: "Has a uppercase Letter", regex: /[A-Z]/ },
+  consecutiveLetters: { name: "Has NO consecutive letters", regex: /^(?!.*([a-zA-Z])\1)/, },
+};
+
+export function Input({ manualRequirement = [], requirements = [] }: InputProps) {
+  const selectedRequirements = requirements.map((req) => defaultPasswordRequirements[req]);
+  const [password, setPassword] = useState("");
+
+  const validations = [...selectedRequirements, ...manualRequirement];
+
   return (
     <div className="flex gap-3 max-sm:flex-col">
       <div>
-        <input />
+        <input onChange={(e) => setPassword(e.target.value)} />
       </div>
 
       <div className="flex flex-col gap-3">
-        {array.map((item, index) => (
-          <div className="flex gap-3 items-center" key={index}>
-            <Image
-              src={false ? "check-icon.svg" : "wrong-icon.svg"}
-              height={30}
-              width={30}
-              alt="img"
-            />
+        {validations.map((item, index) => {
+          const isRequirementValidated = item.regex.test(password);
 
-            <span>{item.name}</span>
-          </div>
-        ))}
+          return (
+            <div className="flex gap-3 items-center" key={index}>
+              <Image
+                src={isRequirementValidated ? "check-icon.svg" : "wrong-icon.svg"}
+                height={30}
+                width={30}
+                alt="img"
+              />
+
+              <span>{item.name}</span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
